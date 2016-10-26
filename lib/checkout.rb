6778172ee79a6669170620423102c25bbb56a1af
Javier Pedreira <javier@JAVIER-PC.Home>
2016-10-26 23:54:49 +0100

    Add total to checkout to pass some tests

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,14 @@
+require 'product'
+
 class Checkout
+  attr_reader :total
+
+  def initialize()
+    @total = 0
+  end
+
+  def scan(product_value)
+
+    @total += product_value
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,5 +1,25 @@
 require 'checkout'
 
 describe Checkout do
+  subject(:checkout) { Checkout.new }
+  subject(:product) { 9.25 }
+  describe '#scan' do
+    context "I haven't scanned any product" do
+      before do
+        checkout.scan(product) 
+      end
 
+      it 'increases the total by the product value' do
+        expect(checkout.total).to eql(9.25)
+      end
+    end
+  end
+
+  describe '#total' do
+    context "I haven't scanned any product" do
+      it 'returns zero' do
+        expect(checkout.total).to eql(0)
+      end 
+    end
+  end
 end
